f91b3045fb093c413e04b4c35d8249136a8b9467
Jameldaykah <meldaykah@yahoo.com>
2024-03-31 13:50:42 -0400

    Jamel Changes

--- ForGroup13/1/Java Files/TrainingRecordViewer.java
+++ ForGroup13/1/Java Files/TrainingRecordViewer.java
@@ -11,31 +11,30 @@ public class TrainingRecordViewer {
     private static final String PASSWORD = "Cronan103!";
 
     public void viewTrainingRecord(String employeeId) throws SQLException {
-        Connection conn = getConnection();
-        PreparedStatement stmt = conn.prepareStatement(
-                "SELECT e.first_name, e.last_name, t.course_title, t.has_exam " +
-                        "FROM employeedata e " +
-                        "INNER JOIN employee_training et ON e.employee_id = et.employee_id " +
-                        "INNER JOIN training_courses t ON et.course_id = t.course_id " +
-                        "WHERE e.employee_id = ?");
-        stmt.setString(1, employeeId);
-        ResultSet rs = stmt.executeQuery();
+        try (Connection conn = getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement(
+                    "SELECT e.first_name, e.last_name, t.course_title, t.has_exam " +
+                            "FROM employeedata e " +
+                            "INNER JOIN employee_training et ON e.employee_id = et.employee_id " +
+                            "INNER JOIN training_courses t ON et.course_id = t.course_id " +
+                            "WHERE e.employee_id = ?");
+            stmt.setString(1, employeeId);
+            ResultSet rs = stmt.executeQuery();
 
-        System.out.println("Employee Name: " + employeeId);
-        System.out.println("Assigned Training Courses:");
-        while (rs.next()) {
-            String courseTitle = rs.getString(3);
-            boolean hasExam = rs.getBoolean(4);
-            System.out.println("- " + courseTitle);
-            if (hasExam) {
-                checkExamResult(employeeId, courseTitle);
+            System.out.println("Employee Name: " + employeeId);
+            System.out.println("Assigned Training Courses:");
+            while (rs.next()) {
+                String courseTitle = rs.getString(3);
+                boolean hasExam = rs.getBoolean(4);
+                System.out.println("- " + courseTitle);
+                if (hasExam) {
+                    checkExamResult(employeeId, courseTitle);
+                }
             }
         }
-
-        conn.close();
     }
 
-    public static String checkExamResult(String employeeId, String courseTitle, int score) throws SQLException {
+    public String checkExamResult(String employeeId, String courseTitle, int score) throws SQLException {
         if (score < 0 || score > 100) {
             throw new IllegalArgumentException("Invalid exam score: " + score);
         }
@@ -47,11 +46,10 @@ public class TrainingRecordViewer {
     }
 
     private Connection getConnection() throws SQLException {
-        try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw e;
-        }
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 }
+
+/*Tester's comments - test was already implemented by team. Minor changes made due to class interaction with external database. 
+  Sql query modified to retrieve employee data
+*/
